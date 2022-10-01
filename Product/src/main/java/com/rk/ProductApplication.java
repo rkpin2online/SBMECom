0c2db3a7ad871dfe03d15146a5c734bf326b0e3c
rk <rkpin2online@gmail.com>
2022-10-01 13:17:09 +0530

    removed sopln

--- Product/src/main/java/com/rk/ProductApplication.java
+++ Product/src/main/java/com/rk/ProductApplication.java
@@ -12,7 +12,6 @@ public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
-		System.out.println("Hello mr.Rk");
 	}
 
 }
